ad6797e8bce842bc8d55d209c5214ccc7ae699ac
Park jongheum <whdgmawkd@naver.com>
2017-09-13 11:53:40 +0900

    Initial Commit

---
+++ app/src/main/java/kr/hs/yii/make/dkuswcamp/MainActivity.java
@@ -0,0 +1,50 @@
+package kr.hs.yii.make.dkuswcamp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+
+        }
+        mWebView = (WebView)findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                // callback.invoke(String origin, boolean allow, boolean remember);
+                callback.invoke(origin, true, true);
+            }
+        });
+        mWebView.loadUrl(getString(R.string.webapp_url));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }else{
+            finish();
+        }
+    }
+}
